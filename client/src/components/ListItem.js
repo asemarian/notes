2638007dc762b6cde8a816307b1109dbc7297d98
Asem <asem.mahmod@gmail.com>
2021-07-27 22:56:09 +0200

    Change placeholder text everywhere

--- client/src/components/ListItem.js
+++ client/src/components/ListItem.js
@@ -1,24 +1,27 @@
-import React, { useRef } from 'react';
-import styles from '../styles/ListItem.module.css';
+import React, { useRef } from "react";
+import styles from "../styles/ListItem.module.css";
 
 const ListItem = ({ note: { title, body }, setCurrentNote, id, selected }) => {
-    const ref = useRef();
+	const ref = useRef();
 
-    const handleClick = () => {
-        setCurrentNote({ title, body, id });
-        ref.current.scrollIntoView({ behavior: "smooth" });
-    }
+	const handleClick = () => {
+		setCurrentNote({ title, body, id });
+		ref.current.scrollIntoView({ behavior: "smooth" });
+	};
 
-    return (
-        <div
-            className={selected ? `${styles.selected} ${styles.container}` : `${styles.container}`}
-            onClick={handleClick}
-            ref={ref}
-        >
-            <p className={styles.title}>{title || "Untitled Note"}</p>
-            <p className={styles.body}>{body || "No Additional Text"}</p>
-        </div>
-    )
-}
+	return (
+		<div
+			className={
+				selected
+					? `${styles.selected} ${styles.container}`
+					: `${styles.container}`
+			}
+			onClick={handleClick}
+			ref={ref}>
+			<p className={styles.title}>{title || "Untitled"}</p>
+			<p className={styles.body}>{body || "Write something here..."}</p>
+		</div>
+	);
+};
 
 export default ListItem;
--- client/src/components/Note.js
+++ client/src/components/Note.js
@@ -1,53 +1,86 @@
-import React, { useState, useEffect, useRef } from 'react';
-import styles from '../styles/Note.module.css';
+import React, { useState, useEffect, useRef } from "react";
+import styles from "../styles/Note.module.css";
 
-const Note = ({ currentNote: { title, body, id }, updateNote, deleteNote, setCurrentNote, toggleInfo, isMobile }) => {
-    const [noteTitle, setNoteTitle] = useState(title);
-    const [noteBody, setNoteBody] = useState(body);
-    const ref = useRef();
+const Note = ({
+	currentNote: { title, body, id },
+	updateNote,
+	deleteNote,
+	setCurrentNote,
+	toggleInfo,
+	isMobile,
+}) => {
+	const [noteTitle, setNoteTitle] = useState(title);
+	const [noteBody, setNoteBody] = useState(body);
+	const ref = useRef();
 
-    useEffect(() => {
-        setNoteTitle(title);
-        setNoteBody(body);
-        ref.current.focus();
-    }, [title, body, id]);
+	useEffect(() => {
+		setNoteTitle(title);
+		setNoteBody(body);
+		ref.current.focus();
+	}, [title, body, id]);
 
-    const handleTitleChange = (e) => {
-        setNoteTitle(e.target.value);
-        updateNote(id, e.target.value, undefined);
-    }
+	const handleTitleChange = (e) => {
+		setNoteTitle(e.target.value);
+		updateNote(id, e.target.value, undefined);
+	};
 
-    const handleBodyChange = (e) => {
-        setNoteBody(e.target.value);
-        updateNote(id, undefined, e.target.value);
-    }
+	const handleBodyChange = (e) => {
+		setNoteBody(e.target.value);
+		updateNote(id, undefined, e.target.value);
+	};
 
-    const handleDelete = () => {
-        deleteNote(id);
-        setCurrentNote({ title: "", body: "", id: "" });
-    }
+	const handleDelete = () => {
+		deleteNote(id);
+		setCurrentNote({ title: "", body: "", id: "" });
+	};
 
-    return (
-        <div className={styles.container}>
-            <div className={styles.toolbar}>
-                {isMobile ?
-                    <button className={styles.button} title="Go Back" onClick={() => setCurrentNote({ title: "", body: "", id: "" })}>
-                        <i className="fas fa-chevron-left"></i>
-                    </button> : null
-                }
-                <button className={styles.button} title="Note Info" onClick={toggleInfo}>
-                    <i className="fas fa-info-circle"></i>
-                </button>
-                <button onClick={handleDelete} className={styles.button} title="Delete Note">
-                    <i className="fas fa-trash-alt"></i>
-                </button>
-            </div>
-            <div className={styles.wrapper}>
-                <input type="text" value={noteTitle} onChange={handleTitleChange} className={`${styles.title} ${isMobile ? styles.title_mobile : ''}`} ref={ref} placeholder="Note title..."/>
-            </div>
-            <textarea className={`${styles.body} ${isMobile ? styles.body_mobile : ''}`} onChange={handleBodyChange} value={noteBody} placeholder="Write something here..." ></textarea>
-        </div>
-    )
-}
+	return (
+		<div className={styles.container}>
+			<div className={styles.toolbar}>
+				{isMobile ? (
+					<button
+						className={styles.button}
+						title="Go Back"
+						onClick={() =>
+							setCurrentNote({ title: "", body: "", id: "" })
+						}>
+						<i className="fas fa-chevron-left"></i>
+					</button>
+				) : null}
+				<button
+					className={styles.button}
+					title="Note Info"
+					onClick={toggleInfo}>
+					<i className="fas fa-info-circle"></i>
+				</button>
+				<button
+					onClick={handleDelete}
+					className={styles.button}
+					title="Delete Note">
+					<i className="fas fa-trash-alt"></i>
+				</button>
+			</div>
+			<div className={styles.wrapper}>
+				<input
+					type="text"
+					value={noteTitle}
+					onChange={handleTitleChange}
+					className={`${styles.title} ${
+						isMobile ? styles.title_mobile : ""
+					}`}
+					ref={ref}
+					placeholder="Untitled"
+				/>
+			</div>
+			<textarea
+				className={`${styles.body} ${
+					isMobile ? styles.body_mobile : ""
+				}`}
+				onChange={handleBodyChange}
+				value={noteBody}
+				placeholder="Write something here..."></textarea>
+		</div>
+	);
+};
 
 export default Note;
